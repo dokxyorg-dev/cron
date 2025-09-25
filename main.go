453eb24559bedac8a193f5ecd46247861d8c1875
dokxyorg-dev <dokxy.org@gmail.com>
2025-09-25 07:53:12 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"time"
 
@@ -14,8 +16,20 @@ func main() {
 		log.Fatal("PING_URL is not set")
 	}
 
-	for {
-		jobs.Ping(url)
-		time.Sleep(10 * time.Minute)
+	go func() {
+		for {
+			jobs.Ping(url)
+			time.Sleep(10 * time.Minute)
+		}
+	}()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
 	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Ping job is running")
+	})
+	log.Printf("Listening on port %s...", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
